907f55fd111c146c4841db2ca5063d7708d06023
annaceglia01 <113606718+annaceglia01@users.noreply.github.com>
2022-11-18 11:45:38 +0100

    Initial Commit

---
+++ PersonalProfile/PersonalProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalProfileApp.swift
+//  PersonalProfile
+//
+//  Created by Anna Ceglia on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PersonalProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
