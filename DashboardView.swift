399935509e29e697bc8773d323686ecb35ed618b
annaceglia01 <113606718+annaceglia01@users.noreply.github.com>
2022-11-23 20:47:31 +0100

    added the dashboard view

--- DashboardView.swift
+++ DashboardView.swift
@@ -9,21 +9,61 @@ import SwiftUI
 
 struct DashboardView: View {
     @State private var path = NavigationPath()
+    @State var searchText = ""
     
     var body: some View {
         NavigationStack(path: $path) {
-            NavigationLink {
-                ProfileView()
-            } label: {
-                Text("Dashboard")
-            }
-
-        }
-    }
-}
+            Image("Homepage")
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarLeading){
+                        NavigationLink {
+                            ProfileView()
+                            
+                        } label: {
+                            Image("Profile")
+                                .resizable()
+                                .clipShape(Circle())
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40, height: 40)
+                        }
+                        
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(Color(.systemGroupedBackground))
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                TextField("Search ..", text: $searchText)
+                            }//HStack
+                            .foregroundColor(.gray)
+                            .padding()
+                            
+                        }//ZStack
+                        .frame(width: 250,height: 35)
+                        .cornerRadius(13)
+                        .padding()
+                        
+                    }
+                    
+                    ToolbarItemGroup (placement: .navigationBarTrailing) {
+                        Button {
+                            print("Edit tapped!")
+                        } label: {
+                            Image(systemName: "pencil")
+                        }
+                        
+                    }//ToolbarItemGroup
+                    
+                }//toolbar
+            
+        }//NavigationStack
+        
+    }//Body
+    
+}//DashboardView
 
 struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
         DashboardView()
     }
 }
+
